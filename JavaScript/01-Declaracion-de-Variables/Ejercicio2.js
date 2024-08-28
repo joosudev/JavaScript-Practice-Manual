a4d9f8ee750d5174584ad3f2d42033b189c115bf
devwillians <vwillians22@gmail.com>
2024-08-27 21:11:33 -0500

    Cargando Ejercicios

---
+++ JavaScript/01-Declaracion-de-Variables/Ejercicio2.js
@@ -0,0 +1,26 @@
+/** Uso de Diferentes Tipos de Datos
+ (string, number, boolean, null, undefined) */
+
+let nombreCompleto = "Ana María" // Tipo String
+let edadActual = 25 // tipo number
+let estaCasada = true // boolean
+let direccion = undefined // tipo underfine
+let hijos = null // tipo null
+
+// Operaciones simples
+let edadEn5Años = edadActual + 5;
+let mensaje = nombreCompleto + " tendra " + edadEn5Años + " años en 5 años"
+
+// Imprimir en la consola
+console.log("Nombre Completo:", nombreCompleto);
+console.log("Edad Actual", edadActual);
+console.log("¿Está Casada?", estaCasada);
+console.log("Dirección:", direccion); // underfined
+console.log("Hijos:", hijos); // null
+console.log(mensaje);
+
+
+
+
+
+
